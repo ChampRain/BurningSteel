ab261099a979c8a9c4404dfaae2f1263171a67ab
cpalcat <viperjuice2012@gmail.com>
2014-04-26 21:32:46 -0500

    Animations

--- BurningSteel/BurningSteel/Components/Animation.cs
+++ BurningSteel/BurningSteel/Components/Animation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using Microsoft.Xna.Framework;
 
@@ -8,7 +9,7 @@ namespace BurningSteel.Components
 {
     class Animation
     {
-        public enum AnimationKey {down, left, right, up}
+        public enum AnimationKey {down, left, right, up, upleft, upright, downleft, downright }
 
         private Rectangle[] frames;
         private int framesPerSecond;
@@ -40,6 +41,65 @@ namespace BurningSteel.Components
         {
             get { return currentFrame; }
             set { currentFrame = (int) MathHelper.Clamp(value, 0, frames.Length - 1); }
+        }
+
+        public int FrameWidth
+        {
+            get { return frameWidth; }
+        }
+
+        public int FrameHeight
+        {
+            get { return frameHeight; }
+        }
+
+        public Animation(int frameCount, int frameWidth, int frameHeight, int xOffset, int yOffset)
+        {
+            frames = new Rectangle[frameCount];
+            this.frameWidth = frameWidth;
+            this.frameHeight = frameHeight;
+
+            for (int i = 0; i < frameCount; i++)
+            {
+                frames[i] = new Rectangle(xOffset + (frameWidth * i), yOffset, frameWidth, frameHeight);
+            }
+
+            framesPerSecond = 5;
+            Reset();
+        }
+
+        private Animation(Animation animation)
+        {
+            this.frames = animation.frames;
+            framesPerSecond = 5;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            frameTimer += gameTime.ElapsedGameTime;
+
+            if (frameTimer > frameLength)
+            {
+                frameTimer = TimeSpan.Zero;
+                currentFrame = (currentFrame + 1) % frames.Length;
+            }
+        }
+
+        public void Reset()
+        {
+            currentFrame = 0;
+            frameTimer = TimeSpan.Zero;
+        }
+
+        public object Clone()
+        {
+            Animation animationClone = new Animation(this);
+
+            animationClone.frameWidth = this.frameWidth;
+            animationClone.frameHeight = this.frameHeight;
+            animationClone.Reset();
+
+            return animationClone;
         }
     }
 }
